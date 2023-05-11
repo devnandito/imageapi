996d88376bd1538df122547d7665ca3e59d676a6
Diego Saldivar <fhersa@gmail.com>
2023-05-10 20:34:30 -0400

    create func upload image

--- api/image.go
+++ api/image.go
@@ -3,11 +3,8 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
-	"os"
-	"path/filepath"
-	"time"
 
 	"github.com/devnandito/imageapi/models"
 )
@@ -36,31 +33,28 @@ func HandleApiShowImage(w http.ResponseWriter, r *http.Request) {
 func HandleApiCreateImage(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&img)
-	
 	if err != nil {
 		fmt.Fprintf(w, "error: %v", err)
 		return
 	}
 
 	jsonData, err := img.ToJson(img)
-	
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	cli := models.Image{}
-	textBytes := []byte(jsonData)
-	er := img.ToText(textBytes, &cli)
-
-	if er != nil {
-		panic(er)
-	}
+	// im := models.Image{}
+	// textBytes := []byte(jsonData)
+	// er := im.ToText(textBytes, &im)
+	// if er != nil {
+	// 	panic(er)
+	// }
 
 	data := models.Image{
 		Title: img.Title,
 		Description: img.Description,
-		Url: cli.Url,
+		Url: img.Url,
 	}
 
 	response, err := img.CreateImage(&data)
@@ -75,100 +69,6 @@ func HandleApiCreateImage(w http.ResponseWriter, r *http.Request) {
 
 // HandleUploadImage upload an image
 func HandleUploadImage(w http.ResponseWriter, r *http.Request) {
-	// 32 MB is the default used by FormFile() function
-	if err := r.ParseMultipartForm(10 * 1024 * 1024); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-	}
-
-	// Get a reference to the fileHeaders.
-	// They are accessible only after ParseMultipartForm is called
-	files := r.MultipartForm.File["file"]
-
-	var errNew string
-	var http_status int
-
-	for _, fileHeader := range files {
-			// Open the file
-			file, err := fileHeader.Open()
-			if err != nil {
-					errNew = err.Error()
-					http_status = http.StatusInternalServerError
-					break
-			}
-
-			defer file.Close()
-
-			buff := make([]byte, 512)
-			_, err = file.Read(buff)
-			if err != nil {
-					errNew = err.Error()
-					http_status = http.StatusInternalServerError
-					break
-			}
-
-			// checking the content type
-			// so we don't allow files other than images
-			filetype := http.DetectContentType(buff)
-			if filetype != "image/jpeg" && filetype != "image/png" && filetype != "image/jpg" {
-					errNew = "The provided file format is not allowed. Please upload a JPEG,JPG or PNG image"
-					http_status = http.StatusBadRequest
-					break
-			}
-
-			_, err = file.Seek(0, io.SeekStart)
-			if err != nil {
-					errNew = err.Error()
-					http_status = http.StatusInternalServerError
-					break
-			}
-
-			err = os.MkdirAll("./uploads", os.ModePerm)
-			if err != nil {
-					errNew = err.Error()
-					http_status = http.StatusInternalServerError
-					break
-			}
-
-			f, err := os.Create(fmt.Sprintf("./uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
-			if err != nil {
-					errNew = err.Error()
-					http_status = http.StatusBadRequest
-					break
-			}
-
-			defer f.Close()
-
-			_, err = io.Copy(f, file)
-			if err != nil {
-					errNew = err.Error()
-					http_status = http.StatusBadRequest
-					break
-			}
-	}
-	message := "file uploaded successfully"
-	messageType := "S"
-
-	if errNew != "" {
-			message = errNew
-			messageType = "E"
-	}
-
-	if http_status == 0 {
-			http_status = http.StatusOK
-	}
-
-	resp := map[string]interface{}{
-			"messageType": messageType,
-			"message":     message,
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http_status)
-	json.NewEncoder(w).Encode(resp)
-}
-
-// HandleUpload
-func HandleUpload(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(10 * 1024 * 1024)
 	file, handler, err := r.FormFile("myfile")
 
@@ -181,5 +81,115 @@ func HandleUpload(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("File Info")
 	fmt.Println("File Name: ", handler.Filename)
 	fmt.Println("File Size: ", handler.Size)
-	fmt.Println("File Type: ", handler.Header)
+	fmt.Println("File Type: ", handler.Header.Get("Content-Type"))
+
+	// Upload file
+	tempFile, err2 := ioutil.TempFile("uploads", "upload-*.jpg")
+
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	defer tempFile.Close()
+
+	fileBytes, err3 := ioutil.ReadAll(file)
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+	tempFile.Write(fileBytes)
+	fmt.Println("Done")
 }
+
+
+
+// func HandleUploadImage(w http.ResponseWriter, r *http.Request) {
+// 	// 32 MB is the default used by FormFile() function
+// 	if err := r.ParseMultipartForm(10 * 1024 * 1024); err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 	}
+
+// 	// Get a reference to the fileHeaders.
+// 	// They are accessible only after ParseMultipartForm is called
+// 	files := r.MultipartForm.File["file"]
+
+// 	var errNew string
+// 	var http_status int
+
+// 	for _, fileHeader := range files {
+// 			// Open the file
+// 			file, err := fileHeader.Open()
+// 			if err != nil {
+// 					errNew = err.Error()
+// 					http_status = http.StatusInternalServerError
+// 					break
+// 			}
+
+// 			defer file.Close()
+
+// 			buff := make([]byte, 512)
+// 			_, err = file.Read(buff)
+// 			if err != nil {
+// 					errNew = err.Error()
+// 					http_status = http.StatusInternalServerError
+// 					break
+// 			}
+
+// 			// checking the content type
+// 			// so we don't allow files other than images
+// 			filetype := http.DetectContentType(buff)
+// 			if filetype != "image/jpeg" && filetype != "image/png" && filetype != "image/jpg" {
+// 					errNew = "The provided file format is not allowed. Please upload a JPEG,JPG or PNG image"
+// 					http_status = http.StatusBadRequest
+// 					break
+// 			}
+
+// 			_, err = file.Seek(0, io.SeekStart)
+// 			if err != nil {
+// 					errNew = err.Error()
+// 					http_status = http.StatusInternalServerError
+// 					break
+// 			}
+
+// 			err = os.MkdirAll("./uploads", os.ModePerm)
+// 			if err != nil {
+// 					errNew = err.Error()
+// 					http_status = http.StatusInternalServerError
+// 					break
+// 			}
+
+// 			f, err := os.Create(fmt.Sprintf("./uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
+// 			if err != nil {
+// 					errNew = err.Error()
+// 					http_status = http.StatusBadRequest
+// 					break
+// 			}
+
+// 			defer f.Close()
+
+// 			_, err = io.Copy(f, file)
+// 			if err != nil {
+// 					errNew = err.Error()
+// 					http_status = http.StatusBadRequest
+// 					break
+// 			}
+// 	}
+// 	message := "file uploaded successfully"
+// 	messageType := "S"
+
+// 	if errNew != "" {
+// 			message = errNew
+// 			messageType = "E"
+// 	}
+
+// 	if http_status == 0 {
+// 			http_status = http.StatusOK
+// 	}
+
+// 	resp := map[string]interface{}{
+// 			"messageType": messageType,
+// 			"message":     message,
+// 	}
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.WriteHeader(http_status)
+// 	json.NewEncoder(w).Encode(resp)
+// }
--- src/main.go
+++ src/main.go
@@ -9,8 +9,8 @@ func main() {
 	http := server.NewServer(":8080")
 	// API clients
 	http.Handle("GET", "/api/images", api.HandleApiShowImage)
+	http.Handle("POST", "/api/images", api.HandleApiCreateImage)
 	http.Handle("POST", "/api/upload", api.HandleUploadImage)
-	http.Handle("POST", "/upload", api.HandleUpload)
 	// http.Handle("POST", "/api/clients", api.HandleApiCreateClient)
 	// http.Handle("GET", "/api/clients/:id", api.HandleApiPutClient)
 	// http.Handle("POST", "/api/users", handlers.HandleUserPostRequest)
